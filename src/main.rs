eaa8d8a744b720416bf1b1aa89099a8f75341d79
MilesCranmer <miles.cranmer@gmail.com>
2024-04-06 22:56:35 -0400

    Nearly-working unittest

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,15 @@ struct RecordItem<'a> {
 }
 
 fn main() -> ExitCode {
-    if let Err(ref e) = run() {
+    let cli = args::Args::parse();
+    if let Err(ref e) = run(cli) {
         println!("Exception: {}", e);
         return ExitCode::FAILURE;
     }
     ExitCode::SUCCESS
 }
 
-fn run() -> Result<(), Error> {
-    let cli = args::Args::parse();
+fn run(cli: args::Args) -> Result<(), Error> {
         
     // This selects the location of deleted
     // files based on the following order (from
@@ -303,8 +303,9 @@ where
     Ok(())
 }
 
-fn bury<S: AsRef<Path>, D: AsRef<Path>>(source: S, dest: D) -> Result<(), Error> {
+pub fn bury<S: AsRef<Path>, D: AsRef<Path>>(source: S, dest: D) -> Result<(), Error> {
     let (source, dest) = (source.as_ref(), dest.as_ref());
+    println!("Burying to path {}", dest.display());
     // Try a simple rename, which will only work within the same mount point.
     // Trying to rename across filesystems will throw errno 18.
     if fs::rename(source, dest).is_ok() {
@@ -544,3 +545,113 @@ fn delete_lines_from_record<R: AsRef<Path>>(
     }
     Ok(())
 }
+
+
+//////////////////////////////////////////
+// TESTS /////////////////////////////////
+//////////////////////////////////////////
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env::temp_dir;
+    use std::fs::{self, metadata, read_to_string, remove_dir_all, File};
+    use std::io::Write;
+    use std::path::PathBuf;
+    use std::process::Command;
+    use rand::distributions::Alphanumeric;
+    use rand::Rng;
+
+    struct TestEnv {
+        tmpdir: PathBuf,
+        graveyard: PathBuf,
+        src: PathBuf,
+    }
+
+    fn setup_test_env() -> TestEnv {
+        let rand_string = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(16)
+            .map(char::from)
+            .collect::<String>();
+        let tmpdir = temp_dir().join(format!("rip_test_{}", rand_string));
+        let graveyard = tmpdir.join("graveyard");
+        let src = tmpdir.join("data");
+
+        // The graveyard should be created, so we don't test this:
+        //// fs::create_dir_all(&graveyard).unwrap();
+        fs::create_dir_all(&src).unwrap();
+
+        TestEnv { tmpdir, graveyard, src }
+    }
+    fn teardown_test_env(test_env: TestEnv) {
+        let _ = remove_dir_all(test_env.tmpdir);
+    }
+
+    fn make_test_data() -> String {
+        return rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(100)
+            .map(char::from)
+            .collect::<String>();
+    }
+
+    #[test]
+    fn test_bury_unbury() {
+        let data = make_test_data();
+        let test_env = setup_test_env();
+
+        println!("Graveyard dir: {}", test_env.graveyard.display());
+        println!("Src dir: {}", test_env.src.display());
+
+        let datafile_path = test_env.src.join("test_file.txt");
+        let mut file = File::create(&datafile_path).unwrap();
+
+        file.write(data.as_bytes()).unwrap();
+
+        let _ = run(
+            args::Args {
+                targets: [datafile_path.clone()].to_vec(),
+                graveyard: Some(test_env.graveyard.clone()),
+                decompose: false,
+                seance: false,
+                unbury: Option::None,
+                inspect: false,
+            }
+        );
+
+        // Verify that the file no longer exists
+        assert!(!metadata(&datafile_path).is_ok());
+        // Verify that the graveyard exists
+        assert!(metadata(&test_env.graveyard).is_ok());
+
+        // And is now in the graveyard
+        let grave_datafile_path = util::join_absolute(&test_env.graveyard, &datafile_path);
+        // test_env.graveyard.join(&datafile_path);
+        println!("Checking graveyard at {}", grave_datafile_path.display());
+        assert!(metadata(&grave_datafile_path).is_ok());
+        // with the right data
+        let restored_data_from_grave = read_to_string(&datafile_path).unwrap();
+        assert_eq!(restored_data_from_grave, data);
+
+        // Unbury the file using the CLI
+        let output = Command::new("cargo")
+            .arg("run")
+            .arg("--")
+            .arg("--graveyard")
+            .arg(&test_env.graveyard)
+            .arg("-u")
+            .output()
+            .expect("Failed to execute command");
+        assert!(output.status.success());
+
+        // Verify that the file exists in the original location with the correct data
+        assert!(metadata(&datafile_path).is_ok());
+        let restored_data = read_to_string(&datafile_path).unwrap();
+        assert_eq!(restored_data, data);
+
+        // teardown_test_env(test_env)
+    }
+}
+
