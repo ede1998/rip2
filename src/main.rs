53077f97824eed1c8bf4e8cd2e23d90a068ce2fa
MilesCranmer <miles.cranmer@gmail.com>
2024-04-07 03:03:56 -0400

    Add nushell completions as well

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{CommandFactory, Parser, ValueEnum};
 use clap_complete::{generate, Shell};
+use clap_complete_nushell::Nushell;
 use rip;
 use rip::args;
 use std::io::stdout;
@@ -9,11 +10,16 @@ fn main() -> ExitCode {
     let cli = args::Args::parse();
 
     if let Some(shell) = cli.completions.as_deref() {
-        let shell = Shell::from_str(shell, true).unwrap_or_else(|_| {
-            eprintln!("Invalid shell specification: {}", shell);
-            std::process::exit(1);
-        });
-        generate(shell, &mut args::Args::command(), "rip", &mut stdout());
+        if ["nu", "nushell"].contains(&shell) {
+            let shell = Nushell;
+            generate(shell, &mut args::Args::command(), "rip", &mut stdout());
+        } else {
+            let shell = Shell::from_str(shell, true).unwrap_or_else(|_| {
+                eprintln!("Invalid shell specification: {}", shell);
+                std::process::exit(1);
+            });
+            generate(shell, &mut args::Args::command(), "rip", &mut stdout());
+        }
         return ExitCode::SUCCESS;
     }
 
