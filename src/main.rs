dad33a39c983d8b3b237c66e542c1c3932c07a3a
MilesCranmer <miles.cranmer@gmail.com>
2024-04-07 01:54:41 -0400

    Rename package to `rip`

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use rip;
+use rip::args;
+use clap::{CommandFactory, Parser, ValueEnum};
+use clap_complete::{generate, Shell};
+use std::io::stdout;
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    let cli = args::Args::parse();
+
+    if let Some(shell) = cli.completions.as_deref() {
+        let shell = Shell::from_str(shell, true).unwrap_or_else(|_| {
+            eprintln!("Invalid shell specification: {}", shell);
+            std::process::exit(1);
+        });
+        generate(shell, &mut args::Args::command(), "rip", &mut stdout());
+        return ExitCode::SUCCESS;
+    }
+
+    if let Err(ref e) = rip::run(cli) {
+        println!("Exception: {}", e);
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
+}
