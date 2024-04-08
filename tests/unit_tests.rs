b9f228ad9dad44bb14b2eb4d0a10b16e741ecedd
MilesCranmer <miles.cranmer@gmail.com>
2024-04-08 13:42:55 -0400

    Add unit tests for validation

---
+++ tests/unit_tests.rs
@@ -0,0 +1,22 @@
+use rip2::args::{Args, validate_args};
+use rstest::rstest;
+
+
+#[rstest]
+fn test_validation() {
+    let bad_completions = Args {
+        completions: Some("bash".to_string()),
+        decompose: true,
+        ..Args::default()
+    };
+    validate_args(&bad_completions)
+        .expect_err("--completions can only be used by itself");
+
+    let bad_decompose = Args {
+        decompose: true,
+        seance: true,
+        ..Args::default()
+    };
+    validate_args(&bad_decompose)
+        .expect_err("-d,--decompose can only be used with --graveyard");
+}
