03cb955eda1b0c80a4f213972c7047fdf53ef7e2
MilesCranmer <miles.cranmer@gmail.com>
2024-04-07 20:11:28 -0400

    Create tests for environment variables

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,13 +1,14 @@
 use rand::distributions::Alphanumeric;
 use rand::Rng;
 use rstest::rstest;
-use std::env::temp_dir;
+use std::env::{self, temp_dir};
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::PathBuf;
 
-use rip::{args, util};
+use rip::{self, args, util};
 
+#[derive(Debug)]
 struct TestEnv {
     tmpdir: PathBuf,
     graveyard: PathBuf,
@@ -41,6 +42,7 @@ impl TestEnv {
     }
 }
 
+#[derive(Debug)]
 struct TestData {
     data: String,
     path: PathBuf,
@@ -54,9 +56,6 @@ impl TestData {
             .map(char::from)
             .collect::<String>();
 
-        println!("Graveyard dir: {}", test_env.graveyard.display());
-        println!("Src dir: {}", test_env.src.display());
-
         let path = test_env.src.join("test_file.txt");
         let mut file = File::create(&path).unwrap();
         file.write_all(data.as_bytes()).unwrap();
@@ -65,6 +64,8 @@ impl TestData {
     }
 }
 
+/// Test that a file is buried and unburied correctly
+/// Also checks that the graveyard is deleted when decompose is true
 #[rstest]
 #[case::unbury(false)]
 #[case::decomposition(true)]
@@ -75,11 +76,12 @@ fn test_bury_unbury(#[case] decompose: bool) {
     let expected_graveyard_path =
         util::join_absolute(&test_env.graveyard, test_data.path.canonicalize().unwrap());
 
-    let _ = rip::run(args::Args {
+    rip::run(args::Args {
         targets: [test_data.path.clone()].to_vec(),
         graveyard: Some(test_env.graveyard.clone()),
         ..args::Args::default()
-    });
+    })
+    .unwrap();
 
     // Verify that the file no longer exists
     assert!(!test_data.path.exists());
@@ -92,13 +94,14 @@ fn test_bury_unbury(#[case] decompose: bool) {
     let restored_data_from_grave = fs::read_to_string(&expected_graveyard_path).unwrap();
     assert_eq!(restored_data_from_grave, test_data.data);
 
-    let _ = rip::run(args::Args {
+    rip::run(args::Args {
         graveyard: Some(test_env.graveyard.clone()),
         decompose,
         force: decompose,
         unbury: if decompose { None } else { Some(Vec::new()) },
         ..args::Args::default()
-    });
+    })
+    .unwrap();
 
     if decompose {
         // Verify that the graveyard is completely deleted
@@ -111,6 +114,68 @@ fn test_bury_unbury(#[case] decompose: bool) {
         let restored_data = fs::read_to_string(&test_data.path).unwrap();
         assert_eq!(restored_data, test_data.data);
     }
+
+    test_env.teardown();
+}
+
+const ENV_VARS: [&str; 2] = ["GRAVEYARD", "XDG_DATA_HOME"];
+
+// Delete env vars and return them
+// so we can restore them later
+fn cache_and_remove_env_vars() -> [Option<String>; 2] {
+    // This should be the same size as ENV_VARS
+    ENV_VARS.map(|key| {
+        // Check if env var exists
+        let value = env::var(key).ok();
+        env::remove_var(key);
+        value
+    })
+}
+
+/// Test that we can set the graveyard from different env variables
+#[rstest]
+#[case::env_graveyard("GRAVEYARD")]
+#[case::env_xdg_data_home("XDG_DATA_HOME")]
+fn test_graveyard_env(#[case] env_var: &str) {
+    let default_env_vars = cache_and_remove_env_vars();
+    let test_env = TestEnv::new();
+    let test_data = TestData::new(&test_env);
+    let modified_graveyard = if env_var == "XDG_DATA_HOME" {
+        util::join_absolute(&test_env.graveyard, "graveyard")
+    } else {
+        test_env.graveyard.clone()
+    };
+    let expected_graveyard_path =
+        util::join_absolute(modified_graveyard, test_data.path.canonicalize().unwrap());
+
+    println!("Graveyard dir: {}", expected_graveyard_path.display());
+
+    let graveyard = test_env.graveyard.clone();
+    env::set_var(env_var, graveyard);
+
+    rip::run(args::Args {
+        targets: [test_data.path.clone()].to_vec(),
+        // We don't set the graveyard here!
+        ..args::Args::default()
+    })
+    .unwrap();
+
+    assert!(!test_data.path.exists());
+    assert!(test_env.graveyard.exists());
+
+    let restored_data = fs::read_to_string(expected_graveyard_path).unwrap();
+    assert_eq!(restored_data, test_data.data);
+
+    // Iterate over the default env vars and restore them
+    ENV_VARS
+        .iter()
+        .zip(default_env_vars.iter())
+        .for_each(|(key, value)| {
+            env::remove_var(key);
+            if let Some(value) = value {
+                env::set_var(key, value);
+            }
+        });
 
     test_env.teardown();
 }
